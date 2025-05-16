cee54e2c52ec6277a398d8dd025b431e1038bd43
MorganBlack777 <arsenyezhov@mail.ru>
2025-05-16 03:34:34 +0300

    111

---
+++ wsgi.py
@@ -0,0 +1,6 @@
+from main import app
+
+if __name__ == "__main__":
+    # Этот файл используется для запуска приложения через WSGI-сервер
+    # Например, через Gunicorn: gunicorn wsgi:app
+    pass 
